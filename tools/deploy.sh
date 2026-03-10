24154bbcbbede527d205d8d05989023e23c237c4
William C. Canin <william.costa.canin@gmail.com>
2026-03-10 17:11:31 -0300

    Update

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-git add .; git commit -m "Update"; git push origin main; git push lab main
+git add .
+git commit -m "Update"
+git push origin main
+git push lab main
